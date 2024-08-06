78dc95416bec34cd35af42c789cdf434a8f331b4
lioneltonnon <tonnonlionel@gmail.com>
2024-08-06 08:40:21 +0300

    Moved Message class to shared.model package

--- shared/src/main/java/com/lioneltonnon/pigeonpost/shared/Message.java
+++
@@ -1,4 +0,0 @@
-package src.main.java.com.lioneltonnon.pigeonpost.shared;
-
-public class Message {
-}
---
+++ shared/src/main/java/com/lioneltonnon/pigeonpost/shared/model/Message.java
@@ -0,0 +1,4 @@
+package src.main.java.com.lioneltonnon.pigeonpost.shared.model;
+
+public class Message {
+}
