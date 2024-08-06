b901926bd811db68bf1d96f87a38e7a37dcd4035
lioneltonnon <tonnonlionel@gmail.com>
2024-08-06 09:00:45 +0300

    Update Message class to record for immutability

--- shared/src/main/java/com/lioneltonnon/pigeonpost/shared/model/Message.java
+++ shared/src/main/java/com/lioneltonnon/pigeonpost/shared/model/Message.java
@@ -1,4 +1,6 @@
 package src.main.java.com.lioneltonnon.pigeonpost.shared.model;
 
-public class Message {
+import src.main.java.com.lioneltonnon.pigeonpost.shared.interfaces.IMessage;
+
+public record Message(String content) implements IMessage {
 }
