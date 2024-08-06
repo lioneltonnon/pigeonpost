1a097ccca3e06e58b7bc6fa6737a9473ae754666
lioneltonnon <tonnonlionel@gmail.com>
2024-08-06 09:01:19 +0300

    Remove non-record methods from IMessage interface

--- shared/src/main/java/com/lioneltonnon/pigeonpost/shared/interfaces/IMessage.java
+++ shared/src/main/java/com/lioneltonnon/pigeonpost/shared/interfaces/IMessage.java
@@ -1,6 +1,5 @@
 package src.main.java.com.lioneltonnon.pigeonpost.shared.interfaces;
 
 public interface IMessage {
-    String getContent();
-    void setContent(String content);
+    String content();
 }
